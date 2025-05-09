38390a32a508a80f4a6a4ff0727740482c53f466
Junhao Zhao <jzhao@juns-mac-studio.myfiosgateway.com>
2025-05-09 01:10:20 -0400

    revert from using github pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export',
-    images: {
-        unoptimized: true, // if you're using next/image
-    },
-    basePath: '/JunhaoZhao_Portfolio',
-    assetPrefix: '/JunhaoZhao_Portfolio',
 };
 
 export default nextConfig;
