46d88a6cff4e9d245c30272bd66d4f4866a1ff23
jzhao234 <jzhao1155@gmail.com>
2025-11-06 10:38:50 +0900

    added darkmode toggle and added theme context

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import "./globals.css";
 
 import Navbar from "../components/Navbar"
 import Footer from "../components/Footer"
+import { ThemeProvider } from "./context/ThemeContext";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -23,9 +24,11 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
-        <Navbar/>
-        <main className="min-h-screen flex flex-col bg-white dark:bg-[#151516]"> {children} </main>
-        <Footer/>
+        <ThemeProvider>
+          <Navbar/>
+          <main className="min-h-screen flex flex-col bg-white dark:bg-[#151516]"> {children} </main>
+          <Footer/>
+        </ThemeProvider>
       </body>
     </html>
   );
