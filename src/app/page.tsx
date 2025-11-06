436c39dec5e997332b40081d57ffbb4e434ced2a
jzhao234 <jzhao1155@gmail.com>
2025-11-06 13:03:47 +0900

    complete revamp of skills card and update to match current capabilities.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import Experience from "../components/Experience";
-import Technology from "../components/Technology";
+import SkillsCard from "../components/Skills/SkillsCard"
 import Education from "../components/Education"
 import DarkModeToggle from "../components/DarkModeToggle";
 
@@ -136,33 +136,7 @@ export default function Home() {
         
         <div className = "flex-col">
           {/* Tech Stack Start*/}
-          <div className = "m-10 p-3 border-2 border-[#1E90FF]/10 rounded-lg text-black bg-white dark:text-white dark:bg-[#151516] flex-col">
-            {/* Description */}
-            <div>
-              <h2 className = "font-medium text-lg"> Skills </h2>
-            </div>
-
-            <Technology
-              logo = {<Image src = "/logo/javaLogo.svg" alt = "Java" width={40} height={40}/>}
-              name = "Java"
-              version = "Version Java SE 24 (JDK 24)"
-            />
-            <Technology
-              logo = {<Image src = "/logo/cLogo.svg" alt = "Java" width={40} height={40}/>}
-              name = "C"
-              version = "Version C23"
-            />
-            <Technology
-              logo = {<Image src = "/logo/tailwindLogo.svg" alt = "Tailwind CSS" width={40} height={40}/>}
-              name = "Tailwind CSS"
-              version = "Version 4.1"
-            />
-            <Technology
-              logo = {<Image src = "/logo/htmlLogo.svg" alt = "HTMl" width={40} height={40}/>}
-              name = "HTML"
-              version = "Version HTML5"
-            />
-          </div>
+          <SkillsCard/>
           {/* Tech Stack End*/}
 
           {/* Education */}
---
+++ src/components/Skills/SkillsCard.tsx
@@ -0,0 +1,82 @@
+"use client";
+import Image from "next/image";
+
+import SkillsItem from "./SkillsItem";
+
+export default function Skills(){
+    return(
+        <div className = "m-10 p-3 border-2 border-[#1E90FF]/10 rounded-lg text-black bg-white dark:text-white dark:bg-[#151516] flex-col">
+            <h1 className="text-2xl font-bold mb-2"> Skills </h1>
+            {/* Languages */}
+            <h2 className="text-xl font-semibold mb-2"> Languages </h2>
+            <SkillsItem
+                name = "Python"
+                section = "Language"
+            />
+            <SkillsItem
+                name = "C"
+                section = "Language"
+            />
+            <SkillsItem
+                name = "Java"
+                section = "Language"
+            />
+            <SkillsItem
+                name = "JavaScript"
+                section = "Language"
+            />
+            <SkillsItem
+                name = "TypeScript"
+                section = "Language"
+            />
+            <SkillsItem
+                name = "HTML5"
+                section = "Language"
+            />
+            <h2 className="text-xl font-semibold mb-2"> Frameworks </h2>
+            {/* Frameworks */}
+            <h3 className="text-l font-medium mb-2 ml-2"> Frontend </h3>
+            {/* Frontends */}
+            <SkillsItem
+                name = "React"
+                section = "Frontend"
+            />
+            <SkillsItem
+                name = "Next.js"
+                section = "Frontend"
+            />
+            <SkillsItem
+                name = "Tailwind CSS"
+                section = "Frontend"
+            />
+            <SkillsItem
+                name = "D3.js"
+                section = "Frontend"
+            />
+            <SkillsItem
+                name = "Plotly"
+                section = "Frontend"
+            />
+            <h3 className="text-l font-medium mb-2 ml-2"> Backend </h3>
+            {/* Backends */}
+            <SkillsItem
+                name = "FastAPI"
+                section = "Backend"
+            />
+            <SkillsItem
+                name = "Flask"
+                section = "Backend"
+            />
+            {/* Databases & Cloud */}
+            <h2 className="text-xl font-semibold mb-2"> Database & Cloud </h2>
+            <SkillsItem
+                name = "PostgreSQL"
+                section = "Database-Cloud"
+            />
+            <SkillsItem
+                name = "AWS"
+                section = "Database-Cloud"
+            />
+        </div>
+    );
+}
