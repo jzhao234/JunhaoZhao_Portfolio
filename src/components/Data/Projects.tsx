b6cdf45ec80aac037c43ddb9a765901884956b4a
jzhao234 <jzhao1155@gmail.com>
2026-02-25 19:11:10 -0500

    added baketsu project and relevant screenshot asset.

--- src/components/Data/Projects.tsx
+++ src/components/Data/Projects.tsx
@@ -18,7 +18,18 @@ export const projects = [
       name: "Drug Synergy Finder",
       description1: "- Built a full-stack web application to calculate and visualize drug synergies for cancer treatment research",
       description2: "- Used Next.js, TypeScript and Tailwind CSS to build the frontend and FastAPI and Python for the backend",
-      description4: "- Visualize results using Plotly and D3.js for researchers to quickly analyze results with a comprehensive SAPE score for a subjective overview",
+      description3: "- Visualize results using Plotly and D3.js for researchers to quickly analyze results with a comprehensive SAPE score for a subjective overview",
       skills: ["Next.js", "Python", "TypeScript", "Tailwind CSS", "Plotly", "D3.js", "FastAPI"], 
     },
+    {
+      id: "Baketsu",
+      image: (<Image src="/projects/home page.png" alt="Baketsu Screenshot" width={300} height={300} />),
+      name: "Baketsu",
+      description1: "- Developed a full-stack cloud storage platform for scalable file storage utilizing aws s3",
+      description2: "- Implemented user authentication flows including registration, login, JWT handling, password hashing (bcrypt), protected routes, and backend-generated email verification tokens",
+      description3: "- Built file preview endpoints to generate temporary S3 URLs for secure thumbnail rendering and improved file management UX.",
+      description4: "- Designed relational database models using SQLite to track file metadata, size, folder structure, and per-user storage usage for accurate account management",
+      skills: ["Next.js", "Python", "FastAPI", "Tailwind CSS", "SQLite", "Bcrypt", "AWS Amazon S3"],
+      githubLink: "https://github.com/jzhao234/Baketsu-CloudStorage",
+    }
   ];
