2f29376c2fc6f562d411a2dc26bb9ac6e13fb68d
Junhao Zhao <junzhao234@gmail.com>
2026-03-05 01:03:18 -0500

    added functionality to close sorting menu for projects in desktop mode. added different default open state depending on screen size. reorganized code and changed formatting slightly.

--- src/components/Project/ProjectCard.tsx
+++ src/components/Project/ProjectCard.tsx
@@ -29,63 +29,80 @@ export default function ProjectCard() {
     return bMatches - aMatches;
   });
 
-  const [isOpen, setIsOpen] = useState(false);
-  
-  const toggleMenu = () => setIsOpen(!isOpen);
+  const [isOpen, setIsOpen] = useState(true);
+  const [isOpenMobile, setIsOpenMobile] = useState(false)
+
+  const toggleMenu = () => setIsOpen(!isOpen)
+  const toggleMenuMobile = () => setIsOpenMobile(!isOpenMobile)
 
   return (
     <div className="max-w-[95vw] mx-auto mt-5 p-5 flex flex-col justify-center">
       <h1 className="text-2xl mb-3 font-bold"> Projects </h1>
 
-      <button onClick={toggleMenu} className="md:hidden max-w-fit flex items-center p-2 rounded hover:bg-[#1E90FF]/10">
-        {isOpen ? (
-          <Image
-            src="/icons/closeButton.svg"
-            alt="Close menu"
-            width={28}
-            height={28}
-            className="cursor-pointer"
-          />
-        ) : (
-          <Image
-            src="/icons/hamburgerMenu.svg"
-            alt="Open menu"
-            width={28}
-            height={28}
-            className="cursor-pointer"
-          />
-        )}
-        <p className="ml-3 font-semibold"> Sort Projects </p> 
-      </button>
-
-      <div className="hidden md:flex flex-wrap mt-3 gap-3 mb-6">
-        <div className="flex items-center">
-          <Image
+      {/* Display Sorting Options Mobile */}
+      <div className="flex flex-wrap gap-3 mb-6 mt-3">
+        <button onClick={toggleMenuMobile} className="md:hidden flex items-center">
+          {isOpenMobile ? (
+            <Image
+              src="/icons/closeButton.svg"
+              alt="Close menu"
+              width={28}
+              height={28}
+              className="cursor-pointer hover:bg-[#1E90FF]/10"
+            />
+          ) : (
+            <Image
               src="/icons/hamburgerMenu.svg"
               alt="Open menu"
               width={28}
               height={28}
-              className="cursor-pointer"
+              className="cursor-pointer hover:bg-[#1E90FF]/10"
             />
+          )}
           <p className="ml-3 font-semibold"> Sort Projects </p>
-        </div>
-        {allSkills.map(skill => (
-          <button
-            key={skill}
-            onClick={() => toggleSkill(skill)}
-            className={`px-3 py-1 rounded-2xl border-2 
-              ${selectedSkills.includes(skill)
-                ? "bg-[#1E90FF] text-white border-[#1E90FF] font-bold"
-                : "bg-[1E90FF] dark:bg-[#1E1E1E] border-[#1E90FF]/20 dark:border-[#1E90FF]/20"}`}
-          >
-            {skill}
-          </button>
-        ))}
+        </button>
+        {isOpenMobile && (
+          <div className="flex flex-wrap bg-white dark:bg-[#151516] gap-3 md:hidden">
+            {allSkills.map(skill => (
+              <button
+                key={skill}
+                onClick={() => toggleSkill(skill)}
+                className={`px-3 py-1 rounded-2xl border-2
+                  ${selectedSkills.includes(skill)
+                    ? "bg-[#1E90FF] text-white border-[#1E90FF] font-bold"
+                    : "bg-[1E90FF] dark:bg-[#1E1E1E] border-[#1E90FF]/20 dark:border-[#1E90FF]/20"}`}
+              >
+                {skill}
+              </button>
+            ))}
+          </div>
+        )}
       </div>
 
-      {isOpen && (
-        <div className="top-16 left-0 w-full bg-white dark:bg-[#151516] flex flex-col items-center space-y-4 py-6 md:hidden">
-          <div className="flex flex-wrap gap-3 mb-6">
+      {/* Display Sorting Desktop */}
+      <div className="hidden md:flex flex-wrap gap-3 mb-6">
+        <button onClick={toggleMenu} className="flex items-center"> 
+          {isOpen ? (
+            <Image
+              src="/icons/closeButton.svg"
+              alt="Close menu"
+              width={28}
+              height={28}
+              className="cursor-pointer hover:bg-[#1E90FF]/10"
+            />
+          ) : (
+            <Image
+              src="/icons/hamburgerMenu.svg"
+              alt="Open menu"
+              width={28}
+              height={28}
+              className="cursor-pointer hover:bg-[#1E90FF]/10"
+            />
+          )}
+          <p className="ml-3 font-semibold"> Sort Projects </p>
+        </button>
+        {isOpen && (
+          <div className="flex flex-wrap bg-white dark:bg-[#151516] gap-3">
             {allSkills.map(skill => (
               <button
                 key={skill}
@@ -99,9 +116,10 @@ export default function ProjectCard() {
               </button>
             ))}
           </div>
-        </div>
-      )}
+        )}
+      </div>
 
+      {/* Display Projects */}
       <div className="flex flex-wrap justify-center gap-6">
         {displayOrder.map((project) => (
           <ProjectItem key={project.id} {...project} selectedSkills={selectedSkills} />
