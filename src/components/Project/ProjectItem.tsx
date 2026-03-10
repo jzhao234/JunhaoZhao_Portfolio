b1ae1ec6e20d62b3231974769a8d82d06bffaf87
jzhao234 <jzhao1155@gmail.com>
2026-03-10 18:27:54 -0400

    changed arrow down icon to ... icon for more info

--- src/components/Project/ProjectItem.tsx
+++ src/components/Project/ProjectItem.tsx
@@ -75,8 +75,8 @@ export default function ProjectItem({id, slug, images, name, description1, descr
                         />
                     </div>
                     <div className = "relative group flex justify-end items-center text-[#1E90FF] bg-white dark:bg-[#171718] rounded-xl p-1">
-                        <svg xmlns="http://www.w3.org/2000/svg" height="25px" viewBox="0 -960 960 960" width="25px" fill="currentColor" className = {`transform transition-transform duration-300`}>
-                            <path d="M480-344 240-584l56-56 184 184 184-184 56 56-240 240Z"/>
+                        <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#1E90FF">
+                            <path d="M240-400q-33 0-56.5-23.5T160-480q0-33 23.5-56.5T240-560q33 0 56.5 23.5T320-480q0 33-23.5 56.5T240-400Zm240 0q-33 0-56.5-23.5T400-480q0-33 23.5-56.5T480-560q33 0 56.5 23.5T560-480q0 33-23.5 56.5T480-400Zm240 0q-33 0-56.5-23.5T640-480q0-33 23.5-56.5T720-560q33 0 56.5 23.5T800-480q0 33-23.5 56.5T720-400Z"/>
                         </svg>
                         <span className="w-fit whitespace-nowrap bg-white dark:bg-[#171718] p-1 px-2 rounded-2xl absolute hidden group-hover:block -translate-y-8 translate-x-18">
                             More Details
