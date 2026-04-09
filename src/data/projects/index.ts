35373925778722aca51192203b58b08d3dc5f898
Junhao Zhao <junzhao234@gmail.com>
2026-04-09 03:58:26 -0400

    removed unused import after deleting portfolio

--- src/data/projects/index.ts
+++ src/data/projects/index.ts
@@ -1,9 +1,8 @@
 export type { ProjectTypes } from "./types";
 
-import portfolio from "./portfolio";
 import drugSynergy from "./drug-synergy";
 import baketsu from "./baketsu";
 import osakaHibachi from "./osaka-hibachi";
 import soraAscent from "./sora-ascent";
 
-export const projects = [portfolio, drugSynergy, baketsu, osakaHibachi, soraAscent];
+export const projects = [drugSynergy, baketsu, osakaHibachi, soraAscent];
